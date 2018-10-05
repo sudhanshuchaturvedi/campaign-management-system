a5cbe3b22df7cccfd7e24f2622168caeec8c510a
Sudhanshu Chaturvedi1 <sudhanshu.c@SLS.loc>
2018-10-05 23:02:50 +0530

    first commit

---
+++ src/main/java/com/camapaign/application/CampaignManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package com.camapaign.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignManagementSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/com/camapaign/application/CampaignManagementSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.camapaign.application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CampaignManagementSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
